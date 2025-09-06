c7243529eb1feee86cacafa120f1ae5797bf38fa
aprimus <amerigo.armentano@hotmail.it>
2025-09-06 14:38:11 +0200

    wip

--- app/parser.py
+++ app/parser.py
@@ -5,6 +5,7 @@ from os import PathLike
 __all__ = ["SqliteParser"]
 
 
+
 class SqliteParser:
     AVAILABLE_COMMANDS = [".dbinfo", ".tables",]
 
@@ -46,6 +47,45 @@ class SqliteParser:
             page_count = struct.unpack(">H", db_file.read(2))[0]
             print("number of tables: ", page_count)
 
+    @classmethod
+    def get_serial_type_code(cls, n: int) -> int | str:
+        match n:
+            case 0:
+                return "NULL"
+            case 1:
+                return 1
+            case 2:
+                return 2
+            case 3:
+                return 3
+            case 4:
+                return 4
+            case 5:
+                return 6
+            case 6:
+                return 8
+            case 7:
+                return 8
+            case 8:
+                return 0
+            case 9:
+                return 0
+            case 10 | 11:
+                return "VARIABLE"
+            case _ if n >= 12 and n % 2 == 0:
+                return (n - 12) // 2
+            case _ if n >= 13 and n % 2 != 0:
+                return (n - 13) // 2
+            case _:
+                raise ValueError(f"Invalid serial type code: {n}")
+
+    def decode_record(self, buffer):
+        print("---- RECORD HEADER----")
+        record_header_size = self.get_varint(buffer)
+        schema_type_size = self.get_serial_type_code(self.get_varint(buffer))
+        print(schema_type_size)
+
+
     def tables(self):
         with self as db_file:
             # -- file header
@@ -62,4 +102,7 @@ class SqliteParser:
             for _ in range(nr_cells):
                 record_size = self.get_varint(db_file)
                 row_id = self.get_varint(db_file)
+                print(record_size)
+                print(row_id)
+                self.decode_record(db_file)
                 break
